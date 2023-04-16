e929a9b1636ee36cd01f6dc3a688d1a5383f6723
yogis <yogis@DESKTOP-9K7JFJ4>
2023-04-16 10:26:11 +0530

    commit and pushing to repository

---
+++ Actitime/src/com/actitime/generic/BaseClass.java
@@ -0,0 +1,10 @@
+package com.actitime.generic;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Actitime/src/com/actitime/pom/LoginPage.java
@@ -0,0 +1,10 @@
+package com.actitime.pom;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
